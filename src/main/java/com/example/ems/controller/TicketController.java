e3d9f26f337523a7b589aa95ae5bef0bc79375bf
Harsh Nakrani <nakraniharsh1412@gmail.com>
2025-07-18 16:05:18 +0530

    unwanted logs deleted from controller and pom.xml

--- src/main/java/com/example/ems/controller/TicketController.java
+++ src/main/java/com/example/ems/controller/TicketController.java
@@ -3,11 +3,8 @@ package com.example.ems.controller;
 import com.example.ems.dto.TicketDto;
 import com.example.ems.dto.TicketResponseDto;
 import com.example.ems.service.TicketService;
-<<<<<<< HEAD
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-=======
->>>>>>> 4b5862a1090311a5d42656c6413cc2b3a7a1d57b
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -24,22 +21,16 @@ public class TicketController {
 
     @PostMapping
     @PreAuthorize("hasRole('USER')")
-<<<<<<< HEAD
     @SecurityRequirement(name = "Bearer Token")
     @Operation(summary = "Create a new ticket")
-=======
->>>>>>> 4b5862a1090311a5d42656c6413cc2b3a7a1d57b
     public TicketDto createTicket(@Valid @RequestBody TicketDto ticketDto) {
         return ticketService.createTicket(ticketDto);
     }
 
     @GetMapping("/event/{eventId}")
     @PreAuthorize("hasRole('USER')")
-<<<<<<< HEAD
     @SecurityRequirement(name = "Bearer Token")
     @Operation(summary = "Get tickets by event ID")
-=======
->>>>>>> 4b5862a1090311a5d42656c6413cc2b3a7a1d57b
     public List<TicketDto> getTicketsByEvent(@PathVariable String eventId) {
         return ticketService.getTicketsByEvent(eventId);
     }
@@ -47,33 +38,24 @@ public class TicketController {
     @GetMapping("/user/{userId}")
 //    @PreAuthorize("hasRole('USER')")
     @PreAuthorize("hasPermission(#userId, 'USER')")
-<<<<<<< HEAD
     @SecurityRequirement(name = "Bearer Token")
     @Operation(summary = "Get tickets by user ID")
-=======
->>>>>>> 4b5862a1090311a5d42656c6413cc2b3a7a1d57b
     public List<TicketDto> getTicketsByUser(@PathVariable String userId) {
         return ticketService.getTicketsByUser(userId);
     }
 
     @DeleteMapping("/{ticketId}")
     @PreAuthorize("hasRole('USER')")
-<<<<<<< HEAD
     @SecurityRequirement(name = "Bearer Token")
     @Operation(summary = "Delete a ticket by ID")
-=======
->>>>>>> 4b5862a1090311a5d42656c6413cc2b3a7a1d57b
     public void deleteTicket(@PathVariable String ticketId) {
         ticketService.deleteTicket(ticketId);
     }
 
     @GetMapping("/all")
     @PreAuthorize("hasRole('USER')")
-<<<<<<< HEAD
     @SecurityRequirement(name = "Bearer Token")
     @Operation(summary = "Get all tickets")
-=======
->>>>>>> 4b5862a1090311a5d42656c6413cc2b3a7a1d57b
     public List<TicketResponseDto> getTicketById() {
         return ticketService.getTicketDetailsById();
     }

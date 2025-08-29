06ca32722eeecd76479fb1497cc1c3071cc6f9de
Harsh Nakrani <nakraniharsh1412@gmail.com>
2025-08-29 10:24:36 +0530

    Changes in all controllers (minor changes).

--- src/main/java/com/example/ems/controller/EventController.java
+++ src/main/java/com/example/ems/controller/EventController.java
@@ -2,7 +2,7 @@ package com.example.ems.controller;
 
 import com.example.ems.dto.EventDto;
 import com.example.ems.dto.LocationDto;
-import com.example.ems.service.EventServiceImpl;
+import com.example.ems.service.EventService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
@@ -16,14 +16,14 @@ import java.util.List;
 @RequestMapping("/api/events")
 public class EventController {
 
-    private final EventServiceImpl eventServiceImpl;
+    private final EventService eventService;
 
     @PostMapping
     @PreAuthorize("hasRole('ORGANIZER')")
     @SecurityRequirement(name = "Bearer Token")
     @Operation(summary = "Create a new event")
     public EventDto createEvent(@RequestBody EventDto eventDto) {
-        return eventServiceImpl.createEvent(eventDto);
+        return eventService.createEvent(eventDto);
     }
 
     @GetMapping("/all")
@@ -31,7 +31,7 @@ public class EventController {
     @SecurityRequirement(name = "Bearer Token")
     @Operation(summary = "Get all events")
     public List<EventDto> getAllEvent() {
-        return eventServiceImpl.getAllEvents();
+        return eventService.getAllEvents();
     }
 
     @GetMapping("/organizer/{name}")
@@ -39,7 +39,7 @@ public class EventController {
     @SecurityRequirement(name = "Bearer Token")
     @Operation(summary = "Get events by organizer name")
     public List<EventDto> getEventsByOrganizer(@PathVariable String name) {
-        return eventServiceImpl.getEventsByOrganizer(name);
+        return eventService.getEventsByOrganizer(name);
     }
 
     @GetMapping("/location")
@@ -47,7 +47,7 @@ public class EventController {
     @SecurityRequirement(name = "Bearer Token")
     @Operation(summary = "Get events by location")
     public List<EventDto> getEventsByLocation(@RequestParam double latitude, @RequestParam double longitude) {
-        return eventServiceImpl.getEventByLocation(new LocationDto(latitude, longitude));
+        return eventService.getEventByLocation(new LocationDto(latitude, longitude));
     }
 
     @GetMapping("/{eventId}")
@@ -55,7 +55,7 @@ public class EventController {
     @SecurityRequirement(name = "Bearer Token")
     @Operation(summary = "Get event by ID")
     public EventDto getEventById(@PathVariable String eventId) {
-        return eventServiceImpl.getEventDtoById(eventId);
+        return eventService.getEventDtoById(eventId);
     }
 
     @PatchMapping("/update")
@@ -63,7 +63,7 @@ public class EventController {
     @SecurityRequirement(name = "Bearer Token")
     @Operation(summary = "Update an existing event")
     public EventDto updateEvent(@RequestBody EventDto eventDto) {
-        return eventServiceImpl.updateEvent(eventDto);
+        return eventService.updateEvent(eventDto);
     }
 
     @DeleteMapping("/{eventId}")
@@ -71,7 +71,7 @@ public class EventController {
     @SecurityRequirement(name = "Bearer Token")
     @Operation(summary = "Delete an event by ID")
     public void deleteEvent(@PathVariable String eventId) {
-        eventServiceImpl.deleteEvent(eventId);
+        eventService.deleteEvent(eventId);
     }
 
     @GetMapping({"/search", "/search/{eventName}", "/search/"})
@@ -79,7 +79,7 @@ public class EventController {
     @SecurityRequirement(name = "Bearer Token")
     @Operation(summary = "Search events by name")
     public List<EventDto> searchEvents(@PathVariable(required = false) String eventName) {
-        return eventServiceImpl.searchEventsByName(eventName);
+        return eventService.searchEventsByName(eventName);
     }
 
 }
--- src/main/java/com/example/ems/controller/UserController.java
+++ src/main/java/com/example/ems/controller/UserController.java
@@ -3,7 +3,7 @@ package com.example.ems.controller;
 import com.example.ems.dto.UserDto;
 import com.example.ems.dto.request.JwtRequest;
 import com.example.ems.dto.response.JwtResponse;
-import com.example.ems.service.UserServiceImpl;
+import com.example.ems.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
@@ -18,27 +18,27 @@ import java.util.List;
 @RequestMapping("/api")
 public class UserController {
 
-    private final UserServiceImpl userServiceImpl;
+    private final UserService userService;
 
     // Register User
     @Operation(summary = "Register a new user")
     @PostMapping("/register")
     public UserDto registerUser(@Valid @RequestBody UserDto userDto) {
-        return userServiceImpl.registerUser(userDto);
+        return userService.registerUser(userDto);
     }
 
     //login controller with jwt token
     @PostMapping("/login")
     @Operation(summary = "Login user")
     public JwtResponse login(@RequestBody JwtRequest loginRequest) {
-        return userServiceImpl.login(loginRequest.getEmail(), loginRequest.getPassword());
+        return userService.login(loginRequest.getEmail(), loginRequest.getPassword());
     }
 
     @PatchMapping("/update")
     @Operation(summary = "Update user details")
     @SecurityRequirement(name = "Bearer Token")
     public UserDto updateUser(@RequestBody UserDto userDto) {
-        return userServiceImpl.updateUser(userDto);
+        return userService.updateUser(userDto);
     }
 
     @SecurityRequirement(name = "Bearer Token")
@@ -46,21 +46,21 @@ public class UserController {
     @Operation(summary = "Get all users")
     @PreAuthorize("hasRole('ORGANIZER')")
     public List<UserDto> getAllUsers() {
-        return userServiceImpl.getAllUsers();
+        return userService.getAllUsers();
     }
 
     @SecurityRequirement(name = "Bearer Token")
     @Operation(summary = "User profile details")
     @GetMapping("/profile")
     public UserDto getMyDetails() {
-        return userServiceImpl.getProfile();
+        return userService.getProfile();
     }
 
     @DeleteMapping("/delete/user/{id}")
     @SecurityRequirement(name = "Bearer Token")
     @Operation(summary = "Delete a user by ID")
     public void deleteUser(@PathVariable String id) {
-        userServiceImpl.deleteUser(id);
+        userService.deleteUser(id);
     }
 
 }
